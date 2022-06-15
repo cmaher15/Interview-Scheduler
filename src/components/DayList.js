eb31344054051e66a402a4677cad336e06902525
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-15 15:07:37 -0700

    refactored and changed prop names per Compass instructions, updated stories

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,7 +7,7 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const days = props.days;
   const mapDays = days.map((day) =>
-    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={props.id === day.id} setDay={props.setDay}/>
+    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange}/>
   );
   return <ul>{mapDays}</ul>;
 }
