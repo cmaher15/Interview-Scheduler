a8cbe6d048f9e64a450499d8bfbaf725aba11efb
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-21 13:27:10 -0700

    removed comment from function after code review w/ mentor

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,7 +5,6 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   function transition(newMode, replace = false) {
     setMode(newMode);
-    //COME BACK - LAST ITEM OF HISTORY SHOULD ALWAYS CHANGE!! 
     setHistory(!replace ? [...history, mode] : [...history]);
   }
   function back() {
