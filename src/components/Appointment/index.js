998fe9be4f045da120b34ba8ab02d93f6cd513db
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-20 18:01:11 -0700

    added error messages for failed promises

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
@@ -15,6 +16,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
   function save(name, interviewer) {
@@ -23,12 +26,18 @@ export default function Appointment(props) {
       interviewer,
     };
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(() => transition(ERROR_SAVE));
   }
 
   function deleteApp(interview) {
     transition(DELETING);
-    props.cancelInterview(props.id, interview).then(() => transition(EMPTY));
+    props
+      .cancelInterview(props.id, interview)
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE));
   }
 
   const { mode, transition, back } = useVisualMode(
@@ -69,6 +78,12 @@ export default function Appointment(props) {
       )}
       {mode === SAVING && <Status message={"Saving..."} />}
       {mode === DELETING && <Status message={"Deleting..."} />}
+      {mode === ERROR_SAVE && (
+        <Error message={"There was an error updating your request."} onClose={back} />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error message={"There was an error deleting your request."} onClose={back}/>
+      )}
     </article>
   );
 }
