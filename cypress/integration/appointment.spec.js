493a3f0ddeccef733476d8e65025a1fb84b77ca2
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-22 14:19:54 -0700

    implemented the edit interview cypress test

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -7,19 +7,28 @@ describe("Appointments", () => {
 
   it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
+
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+
     cy.contains("Save").click();
+    
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
   it("should edit an interview", () => {
-    // Visits the root of our web server
-    // Clicks the edit button for the existing appointment
-    // Changes the name and interviewer
-    // Clicks the save button
-    // Sees the edit to the appointment
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
   it("should cancel an interview", () => {
